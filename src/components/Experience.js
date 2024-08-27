a703893ed300688fd46a4d60234ab90d91e955a2
Tien Chung <tchung7171@gmail.com>
2024-08-27 13:18:01 -0400

    Added Experience section.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,8 +1,39 @@
 import { Container, Row, Col } from "react-bootstrap";
 import 'animate.css';
 import { EXPERIENCE_DES } from "../utils/constants";
+import workImg from '../assets/img/work.jpg';
+import projectImg from '../assets/img/project.jpeg';
+import educationImg from '../assets/img/school.jpeg';
+import ExperienceCard from "./ExperienceCard";
+import { useNavigate } from "react-router-dom";
 
-const Experience = () => {
+const Experience = () => {  
+  const navigate = useNavigate();
+  
+  const education = [
+    {
+      title: "College Journey",
+      description: "BS in Computer Science",
+      imgUrl: educationImg,
+    },
+  ]
+  
+  const work = [
+    {
+      title: "Work Experiences",
+      description: "Full Stack Developer",
+      imgUrl: workImg,
+    },
+  ]
+  
+  const project = [
+    {
+      title: "Personal Projects",
+      description: "Projects",
+      imgUrl: projectImg,
+    },
+  ]
+  
   return (
     <section className="experience" id="Experience">
       <Container>
@@ -11,12 +42,48 @@ const Experience = () => {
         <Row className="center">
           <Col xs={12} md={4}>
             <h3>Education</h3>
+            <button onClick={() => navigate('/Education')}>
+              {
+                education.map((project, index) => {
+                  return (
+                    <ExperienceCard
+                      key={index}
+                      {...project}
+                      />
+                  )
+                })
+              }
+            </button>
           </Col>
           <Col xs={12} md={4}>
             <h3>Work Experience</h3>
+            <button onClick={() => navigate('/Work')}>
+              {
+                work.map((project, index) => {
+                  return (
+                    <ExperienceCard
+                      key={index}
+                      {...project}
+                      />
+                  )
+                })
+              }
+            </button>
           </Col>
           <Col xs={12} md={4}>
             <h3>Personal Projects</h3>
+            <button onClick={() => navigate('/Project')}>
+              {
+                project.map((project, index) => {
+                  return (
+                    <ExperienceCard
+                      key={index}
+                      {...project}
+                      />
+                  )
+                })
+              }
+            </button>
           </Col>
         </Row>
       </Container>
---
+++ src/components/ExperienceCard.js
@@ -0,0 +1,13 @@
+const ExperienceCard = ({ title, description, imgUrl }) => {
+  return (
+      <div className="experience-imgbx">
+        <img src={imgUrl} />
+        <div className="experience-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+  )
+}
+
+export default ExperienceCard;
